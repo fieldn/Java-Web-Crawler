a92749da32fd14dc482242a66c51300741c6136d
Noah Field <fieldn@purdue.edu>
2016-10-18 09:03:11 -0400

    Started on words stuff

--- Crawler.java
+++ Crawler.java
@@ -16,6 +16,19 @@ public class Crawler
 	int urlID;
 	public Properties props;
 
+	List<String> stopWords = Arrays.asList("a","able","about","across","after","all","almost","also",
+		"am","among","an","and","any","are","as","at","be","because","been","but",
+		"by","can","cannot","could","dear","did","do","does","either","else","ever",
+		"every","for","from","get","got","had","has","have","he","her","hers","him",
+		"his","how","however","i","if","in","into","is","it","its","just","least",
+		"let","like","likely","may","me","might","most","must","my","neither","no",
+		"nor","not","of","off","often","on","only","or","other","our","own","rather",
+		"said","say","says","she","should","since","so","some","than","that","the",
+		"their","them","then","there","these","they","this","tis","to","too","twas",
+		"us","wants","was","we","were","what","when","where","which","while","who",
+		"whom","why","will","with","would","yet","you","your");
+	List<String> nonWords = Arrays.asList("<",">","::","|","-",")","(","–");
+
 	Crawler() {
 		urlID = 0;
 		urlQueue = new ConcurrentLinkedDeque<String>();
@@ -74,14 +87,34 @@ public class Crawler
 			return false;
 		else if (link.contains("#"))
 			return false;
+		else if (!link.contains("purdue.edu"))
+			return false;
 		else if (!link.substring(0, 4).equals("http"))
 			return false;
-		else if (!link.contains("purdue.edu"))
+		else if (link.substring(link.length() - 4, link.length()).equals(".PDF"))
 			return false;
 		else if (link.substring(link.length() - 4, link.length()).equals(".pdf"))
 			return false;
+		else if (link.substring(link.length() - 4, link.length()).equals(".doc"))
+			return false;
 		else 
 			return true;
+	}
+
+	public void parseText(TextNode text) {
+		Set<String> words = new HashSet<String>();
+		String[] arr = text.text().toLowerCase().split("[ \"\t\"»•.?,–(&):©/;]");
+		for (String s: arr) {
+			if (stopWords.contains(s) || nonWords.contains(s))
+				continue;
+			else 
+				words.add(s);
+		}
+		for (String s : words) {
+			System.out.print(s + " ");
+		}
+		System.out.println("TEXT OUTPUT");
+		System.out.println(text.text());
 	}
 
 	public void fetchURL() {
@@ -89,6 +122,8 @@ public class Crawler
 			String currentUrl = urlQueue.remove();
 			Document doc = Jsoup.connect(currentUrl).get();
 
+			TextNode text = new TextNode(doc.text(), currentUrl);
+			parseText(text);
 			Elements links = doc.select("a[href]");
 			for (Element l : links) {
 				String link = l.attr("abs:href");
@@ -97,6 +132,7 @@ public class Crawler
 					// Check if it is already found
 					if (!urlFound(link)) {
 						insertURLInDB(link);
+						knownUrls.add(link);
 						urlQueue.add(link);
 						System.out.println(link);
 					}
@@ -116,7 +152,9 @@ public class Crawler
 			String root = crawler.props.getProperty("crawler.root");
 			crawler.createDB();
 			crawler.urlQueue.add(root);
+			crawler.knownUrls.add(root);
 			crawler.fetchURL();
+
 		} catch( Exception e) {
 			e.printStackTrace();
 		}
